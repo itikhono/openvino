61737239200336532706d7bb8ac9a2f3b22e7978
Ivan <ivan.tikhonov@intel.com>
2023-07-12 14:40:23 +0400

    update

--- src/common/transformations/src/transformations/transpose_sinking/ts_strided_slice.cpp
+++ src/common/transformations/src/transformations/transpose_sinking/ts_strided_slice.cpp
@@ -20,16 +20,6 @@ using namespace ov::pass::transpose_sinking::utils;
 
 namespace {
 
-int64_t get_unmodified_order_from(const std::shared_ptr<ov::op::v0::Constant>& transpose_order) {
-    auto order_val = transpose_order->cast_vector<int64_t>();
-    for (int64_t i = static_cast<int64_t>(order_val.size()) - 1; i >= 0; --i) {
-        if (i != order_val[i]) {
-            return i + 1;
-        }
-    }
-    return 0;
-}
-
 std::vector<size_t> convert_mask_to_axis_vec(const std::vector<int64_t>& mask) {
     std::vector<size_t> axes;
     for (size_t i = 0; i < static_cast<size_t>(mask.size()); ++i) {
@@ -58,13 +48,8 @@ TSStridedSliceForward::TSStridedSliceForward() {
             return false;
         }
 
-        // the ordinal number of first element from which Transpose does not change the order of the elements
-        // e.g. transpose_order = (1, 0, 2, 3, 4)
-        // unmodified_order_from = 2, (2, 3, 4) values are arranged in order
-        auto unmodified_order_from = get_unmodified_order_from(transpose_info.transpose_const);
         auto data_rank_val = data_rank.get_length();
-
-        // handle begin, end, stride inputs
+        // align begin, end, stride inputs to insert Transpose for each of them
         std::vector<std::shared_ptr<ov::op::v0::Constant>> new_inputs(3);
         for (size_t input_idx = 1; input_idx <= 3; ++input_idx) {
             auto input = strided_slice->input_value(input_idx);
@@ -75,13 +60,10 @@ TSStridedSliceForward::TSStridedSliceForward() {
             }
 
             auto num_elements = input_pshape[0].get_length();
-            // if input_rank less than the unmodified_order_from,
-            // then we need to extend the input rank to unmodified_order_from number
-            // e.g. transpose_order = (1, 2, 0, 4, 5)
-            // unmodified_order_from = 3
-            // `begin` input rank = 2
-            // we need to add at least one value = 0 to `begin` input to successfully execute Transpose operation
-            if (num_elements < unmodified_order_from) {
+            // if input_rank less than the data input rank,
+            // then we need to extend the input rank to the data input rank
+            // to successfully execute Transpose operation
+            if (num_elements < data_rank_val) {
                 auto input_const = ov::as_type_ptr<ov::op::v0::Constant>(input.get_node_shared_ptr());
                 if (!input_const) {
                     return false;
@@ -90,11 +72,11 @@ TSStridedSliceForward::TSStridedSliceForward() {
                 auto input_const_val = input_const->cast_vector<int64_t>();
                 if (input_idx == 1) {
                     // `begin` input have to be initialized with 0
-                    input_const_val.resize(unmodified_order_from, 0);
+                    input_const_val.resize(data_rank_val, 0);
                 } else if (input_idx == 2) {
                     // 'end' input have to be initialized with the corresponding `data` input dim value
-                    input_const_val.resize(unmodified_order_from);
-                    for (size_t i = (unmodified_order_from - num_elements); i < unmodified_order_from; ++i) {
+                    input_const_val.resize(data_rank_val);
+                    for (size_t i = (data_rank_val - num_elements); i < data_rank_val; ++i) {
                         // todo: we can use int max value and delete this check
                         if (data_partial_shape[i].is_dynamic()) {
                             return false;
@@ -103,19 +85,25 @@ TSStridedSliceForward::TSStridedSliceForward() {
                     }
                 } else {
                     // `stride` input have to be initialized with 1
-                    input_const_val.resize(unmodified_order_from, 1);
+                    input_const_val.resize(data_rank_val, 1);
                 }
                 new_inputs[input_idx-1] = ov::op::v0::Constant::create(input_const->get_element_type(), {input_const_val.size()}, input_const_val);
             }
         }
 
+        // delete Transpose op on 1st input
+        utils::sink_forward::UpdateInputTransposes(main_node, transpose_info, {0});
+
+        auto transpose_order_values = transpose_info.transpose_const->cast_vector<size_t>();
+        auto axis = std::make_shared<ov::op::v0::Constant>(element::i32, Shape{}, 0);
         for (size_t i = 1; i <= new_inputs.size(); ++i) {
             if (new_inputs[i-1]) {
                 strided_slice->input(i).replace_source_output(new_inputs[i-1]);
             }
+            main_node->input(i).replace_source_output(
+                    ChangeValuesOrder(main_node->input_value(i), transpose_order_values, axis));
         }
 
-        auto transpose_order_values = transpose_info.transpose_const->cast_vector<size_t>();
         auto update_mask = [&](std::vector<int64_t> old_mask) {
             old_mask.resize(data_rank_val, 0);
             std::vector<int64_t> new_mask(data_rank_val);
@@ -133,11 +121,6 @@ TSStridedSliceForward::TSStridedSliceForward() {
         auto shrink_axes = convert_mask_to_axis_vec(strided_slice->get_shrink_axis_mask());
         auto new_axes = convert_mask_to_axis_vec(strided_slice->get_new_axis_mask());
 
-        // delete Transpose op on 1st input
-        utils::sink_forward::UpdateInputTransposes(main_node, transpose_info, {0});
-        auto axis = std::make_shared<ov::op::v0::Constant>(element::i32, Shape{}, 0);
-        ChangeValuesOrder(strided_slice->input_value(1), transpose_order_values, axis);
-
         transpose_order_values = GetOrderAfterReduction(shrink_axes, transpose_order_values);
         transpose_order_values = GetOrderBeforeReduction(new_axes, transpose_order_values);
 
@@ -152,6 +135,7 @@ TSStridedSliceForward::TSStridedSliceForward() {
 
     transpose_sinking(matcher_name, sinking_transformation);
 }
+/*
 
 TSStridedSliceBackward::TSStridedSliceBackward() {
     MATCHER_SCOPE(TSStridedSliceBackward);
@@ -277,4 +261,4 @@ TSStridedSliceBackward::TSStridedSliceBackward() {
 
     auto m = std::make_shared<Matcher>(transpose_label, matcher_name);
     register_matcher(m, matcher_pass_callback);
-}
+}*/
